39e1eb16e4e1ad5eef4a8be59cabadd355229533
Ell Neal <elliot@emdentec.com>
2017-12-29 18:29:50 +0000

    Add basic test script; commandeered from jessfraz/dotfiles

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eo pipefail
+
+ERRORS=()
+
+# find all executables and run `shellcheck`
+for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
+    if file "$f" | grep --quiet shell; then
+        {
+            shellcheck "$f" && echo "[OK]: sucessfully linted $f"
+        } || {
+            # add to errors
+            ERRORS+=("$f")
+        }
+    fi
+done
+
+if [ ${#ERRORS[@]} -eq 0 ]; then
+    echo "No errors, hooray"
+else
+    echo "These files failed shellcheck: ${ERRORS[*]}"
+    exit 1
+fi
